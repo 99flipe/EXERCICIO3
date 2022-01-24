57766135bde729a3adeff17cc90e5064d6a950b7
VERTEBR4 <felipesouzadossantos8542@gmail.com>
2022-01-23 22:35:03 -0300

    subindo exercicio 3

---
+++ script.js
@@ -0,0 +1,19 @@
+//1 - Criar um loop que conte de 1 até 10 usando FOR
+for (let contador =1; 10>= contador; contador +=1){
+    console.log(contador)
+}
+//2 - Criar um loop que conte de 10 até 1 usando WHILE
+let contador2 = 10
+while (1<= contador2){
+console.log (contador2)
+contador2 -=1
+}
+//3 - Criar um loop que conte todos os números ímpares de 1 até 100
+for (let cont=1; 100 >= cont; cont +=2){
+    console.log(cont)
+}
+
+//4- Criar um loop que conte todos os números pares de 0 a 100
+for (let cont2 = 0; 100 >= cont2; cont2 +=2) {
+    console.log(cont2)
+}
